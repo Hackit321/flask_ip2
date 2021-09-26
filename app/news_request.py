f61f27bde3ca60cb3c6970538798a1b76cdfb3ae
Hackit321 <kirwamarkk@gmail.com>
2021-09-27 00:28:23 +0300

    created a function that create instance of the config api options

--- app/news_request.py
+++ app/news_request.py
@@ -1,3 +1,18 @@
 from app import app 
 from urllib.request,json
+import requests
+
+
+api_key =None
+s_url = None
+art_url = None
+articles_url =None
+
+
+def configure_request(app):
+    global api_key,s_url,art_url
+    api_key = app.config['NEWS_API_KEY']
+    articles_url =app.config['SOURCE_ARTICLE_URL']
+    s_url = app.config['NEWS_API_BASE_URL']
+    art_url = app.config['NEWS_ARTICLES_API_URL']
 
