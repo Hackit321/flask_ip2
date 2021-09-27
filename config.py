f82033b5b40c5f7958cce45e1b03d24361c3b907
Hackit321 <kirwamarkk@gmail.com>
2021-09-27 05:08:09 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 
 class Config:
-    NEWS_API_KEY ='f4de1ca5f663456ea8bae142093fb813'
     NEWS_API_BASE_URI ='https://newsapi.org/v2/sources?country=us&category={}&apiKey={}'
     NEWS_ARTICLE_API_URL ='https://newsapi.org/v2/everything?q={}&apiKey={}'
     SOURCE_ARTICLE_URL ='https://newsapi.org/v2/everything?sources={}&apiKey='
